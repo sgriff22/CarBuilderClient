af2e192b600e123a33efc4316e07ab91e6a2479a
Stacy Griffy <sgriffy2@gmail.com>
2024-08-30 11:40:37 -0500

    removed unecessary code from database and orders component since api now sends all the info in the json response for total order and each items properties

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,33 +1,16 @@
-import { getWheels, getInteriors, getTechnologies, getPaints, getOrders } from "./database.js"
-
-
-const paints = await getPaints()
-const interiors = await getInteriors()
-const techs = await getTechnologies()
-const wheels = await getWheels()
+import { getOrders } from "./database.js"
 
 export const Orders = async () => {
     const orders = await getOrders()
 
     return `${
         orders.map(order => {
-            const paint = paints.find(p => p.id === order.paintId)
-            const technology = techs.find(t => t.id === order.technologyId)
-            const interior = interiors.find(i => i.id === order.interiorId)
-            const wheel = wheels.find(w => w.id === order.wheelId)
-
             return `<section class="order">
-                ${paint.color} car with
-                ${wheel.style} wheels,
-                ${interior.material} interior,
-                and the ${technology.package}
-                for a total cost of
-                ${
-                    (paint.price + technology.price + interior.price + wheel.price).toLocaleString("en-US", {
-                        style: "currency",
-                        currency: "USD"
-                    })
-                }
+                ${order.paint.color} car with
+                ${order.wheels.style} wheels,
+                ${order.interior.material} interior,
+                and the ${order.technology.package}
+                for a total cost of $${order.totalCost}.
             </section>`
         })
         .join("")
